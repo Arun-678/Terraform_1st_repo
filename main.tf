94f77e2aaf58fce8945bc84f28a942f496a0c8ee
Arun-678 <arun.aws678@gmail.com>
2025-08-30 02:30:51 -0400

    Update main.tf

--- main.tf
+++ main.tf
@@ -2,7 +2,7 @@ provider "aws" {
   region = "us-east-1" # Change region as needed
 }
 
-resource "aws_instance"  {
+resource "aws_instance" "example" {
   ami           = "ami-00ca32bbc84273381" # Amazon Linux 2 AMI in us-east-1 (update per region)
   instance_type = "t2.micro"
 
